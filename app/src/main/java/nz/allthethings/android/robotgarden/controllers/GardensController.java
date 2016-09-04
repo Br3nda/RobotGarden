44d4ac3fbbab883c52a07f8cb41c3566c39b4d0d
Brenda Wallace <brenda@wallace.net.nz>
2016-09-04 19:00:13 +1200

    skeleton of app with login and main activity

---
+++ app/src/main/java/nz/allthethings/android/robotgarden/controllers/GardensController.java
@@ -0,0 +1,37 @@
+package nz.allthethings.android.robotgarden.controllers;
+
+public class GardensController {
+    /*
+                                         gardens GET       /gardens(.:format)                                                            gardens#index
+                                             POST      /gardens(.:format)                                                            gardens#create
+                                  new_garden GET       /gardens/new(.:format)                                                        gardens#new
+                                 edit_garden GET       /gardens/:id/edit(.:format)                                                   gardens#edit
+                                      garden GET       /gardens/:id(.:format)                                                        gardens#show
+                                             PATCH     /gardens/:id(.:format)                                                        gardens#update
+                                             PUT       /gardens/:id(.:format)                                                        gardens#update
+                                             DELETE    /gardens/:id(.:format)                                                        gardens#destroy
+                            gardens_by_owner GET       /gardens/owner/:owner(.:format)                                               gardens#index
+
+     */
+    static public void gardens_by_owner() {
+
+    }
+    static public void create() {
+
+    }
+    static public void New() {
+
+    }
+    static public void show(int id) {
+
+    }
+    static public void update(int id) {
+
+    }
+    static public void destroy(int id) {
+
+    }
+    static public void index() {
+
+    }
+}
---
+++ app/src/main/java/nz/allthethings/android/robotgarden/controllers/SessionsController.java
@@ -0,0 +1,10 @@
+package nz.allthethings.android.robotgarden.controllers;
+
+public class SessionsController {
+    static public void create() {
+
+    }
+    static public boolean isLoggedIn() {
+        return false;
+    }
+}
