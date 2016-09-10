5c06ebe34af19f9c9f08653fcb433be34ead7740
Brenda Wallace <brenda@wallace.net.nz>
2016-09-10 14:55:27 +1200

    default user until find a way to work out logged in user's id

--- app/src/main/java/nz/allthethings/android/robotgarden/config/RobotConfig.java
+++ app/src/main/java/nz/allthethings/android/robotgarden/config/RobotConfig.java
@@ -2,5 +2,6 @@ package nz.allthethings.android.robotgarden.config;
 
 public class RobotConfig {
     public static String url = "http://growstuff.org";
+    public static String owner = "shiny";
 }
 
