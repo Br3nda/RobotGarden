b391fecf37032c0f0407c81bee6a5e0abad91b34
Brenda Wallace <brenda@wallace.net.nz>
2016-09-11 11:42:49 +1200

    Session model has email

--- app/src/main/java/nz/allthethings/android/robotgarden/models/Session.java
+++ app/src/main/java/nz/allthethings/android/robotgarden/models/Session.java
@@ -1,6 +1,14 @@
 package nz.allthethings.android.robotgarden.models;
 
 import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
 
 public class Session extends RealmObject {
+    @PrimaryKey
+    long id;
+    String email;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
