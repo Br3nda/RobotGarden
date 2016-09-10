f4d2eeee023107dd031e42d790dbc0e91e05b938
Brenda Wallace <brenda@wallace.net.nz>
2016-09-10 14:55:16 +1200

    Make a session model when logging in

--- app/src/main/java/nz/allthethings/android/robotgarden/controllers/SessionsController.java
+++ app/src/main/java/nz/allthethings/android/robotgarden/controllers/SessionsController.java
@@ -1,6 +1,7 @@
 package nz.allthethings.android.robotgarden.controllers;
 
 import android.content.Context;
+import android.content.pm.PackageInstaller;
 import android.util.Log;
 
 import com.google.gson.JsonObject;
@@ -9,7 +10,10 @@ import com.koushikdutta.ion.Response;
 
 import java.util.concurrent.ExecutionException;
 
+import io.realm.Realm;
+import io.realm.RealmQuery;
 import nz.allthethings.android.robotgarden.config.RobotConfig;
+import nz.allthethings.android.robotgarden.models.Session;
 
 public class SessionsController {
     private static final String TAG = "Sessions";
@@ -25,11 +29,29 @@ public class SessionsController {
 
         int code = result.getHeaders().code();
 
+        if (code == 200) {
+            createSession();
+        }
+
         Log.d(TAG, "http code = " + code);
         return code == 200;
     }
 
     static public boolean isLoggedIn() {
-        return false;
+        return null != getSession();
+    }
+
+    static private Session getSession() {
+        Realm realm = Realm.getDefaultInstance();
+        Session session = realm.where(Session.class).findFirst();
+        return session;
+    }
+
+    static private Session createSession() {
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+        Session session = realm.createObject(Session.class);
+        realm.commitTransaction();
+        return session;
     }
 }
---
+++ app/src/main/java/nz/allthethings/android/robotgarden/models/Session.java
@@ -0,0 +1,6 @@
+package nz.allthethings.android.robotgarden.models;
+
+import io.realm.RealmObject;
+
+public class Session extends RealmObject {
+}
